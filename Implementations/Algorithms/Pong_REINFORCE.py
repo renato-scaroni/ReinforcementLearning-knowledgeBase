correcting logits computation and removing cpu processing

Implementations/Algorithms/Pong_REINFORCE.py
@@ -99,12 +99,12 @@ class Agent:
 
     def get_action(self, state):
         if self.torch_rand:
-            logits = self.policy_network(state)
-            dist = Categorical(logits=logits)
+            probs = self.policy_network(state)
+            dist = Categorical(probs=probs)
             highest_prob_action = dist.sample()
             log_prob = dist.log_prob(highest_prob_action)
         else:
-            probs = self.policy_network(state).cpu()
+            probs = self.policy_network(state)
             highest_prob_action = np.random.choice(self.num_actions, p=np.squeeze(probs.detach().numpy()))
             log_prob = torch.log(probs.squeeze(0)[highest_prob_action])
 